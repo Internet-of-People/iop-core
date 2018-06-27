0a605cb0d26001111535acda79f5aa59a4901f04
Unknown <jasin95@googlemail.com>
2018-06-27 11:27:34 +0200

    overview grid alignment

--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -220,7 +220,10 @@ void OverviewPage::updateWatchOnlyLabels(bool showWatchOnly)
     //ui->labelSpendable->setVisible(showWatchOnly);      // show spendable label (only when watch-only is active)
     //ui->labelWatchonly->setVisible(showWatchOnly);      // show watch-only label
     //ui->lineWatchBalance->setVisible(showWatchOnly);    // show watch-only balance separator line
-    ui->watchOnly->setVisible(showWatchOnly);
+    ui->watch_available->setVisible(showWatchOnly);
+    ui->watch_total->setVisible(showWatchOnly);
+    ui->watch_pending->setVisible(showWatchOnly);
+
     //ui->labelWatchAvailable->setVisible(showWatchOnly); // show watch-only available balance
     //ui->labelWatchPending->setVisible(showWatchOnly);   // show watch-only pending balance
    // ui->labelWatchTotal->setVisible(showWatchOnly);     // show watch-only total balance
