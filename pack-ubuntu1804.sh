727ddf6f8dc2f063293d3d76b88de5f0364bdff7
Unknown <amon.e@gmx.net>
2018-07-04 21:09:59 +0200

    added pack file for ubuntu 18.04

---
+++ pack-ubuntu1804.sh
@@ -0,0 +1,5 @@
+./pack-common.sh \
+    --type=debian \
+    --pkgrelease=ubuntu1804 \
+    --requires="libprotobuf10,libboost-program-options1.65.1,libboost-filesystem1.65.1,libboost-thread1.65.1,libboost-chrono1.65.1,libqt5gui5,libdb5.3++,libqrencode3,libssl1.0.0,libevent-2.1-6,libevent-pthreads-2.1-6,libminiupnpc10,libzmq5" \
+
