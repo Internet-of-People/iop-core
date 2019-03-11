e8d6362b22873b12fb69adaf84039b24294aae31
Unknown <amon.e@gmx.net>
2019-03-11 12:22:06 +0100

    fixed "jumpback" of difficulty

--- src/pow.cpp
+++ src/pow.cpp
@@ -34,7 +34,7 @@ unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHead
             {
                 // Return the last non-special-min-difficulty-rules-block
                 const CBlockIndex* pindex = pindexLast;
-                while (pindex->pprev && pindex->nHeight % params.DifficultyAdjustmentInterval() != 0 && pindex->nBits == nProofOfWorkLimit)
+                while (pindex->pprev && pindex->nHeight % params.DifficultyAdjustmentInterval() != 0 && pindex->nHeight != params.nSubsidyHalvingInterval && pindex->nBits == nProofOfWorkLimit)
                     pindex = pindex->pprev;
                 return pindex->nBits;
             }
